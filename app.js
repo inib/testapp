90bb469d5fd8cee649aa72a24b42f92e2f408a36
inib <alpinehd@gmail.com>
2016-10-12 07:11:20 +0200

    go

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var app = express();
 app.get('/', function(req, res) {
   res.send('Hello, World!');
 });
- 
+
 var server = app.listen(3000, function() {
  
-console.log('Server listening at http://%s:%s', host, port);
+console.log('Server listening');
  
 });
