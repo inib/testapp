0ad0739ceaaef7f32e28b97dc9ca8f6e1bd686d0
inib <alpinehd@gmail.com>
2016-10-14 00:23:18 +0200

    webhook deploy test

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var express = require('express');
 var app = express();
  
 app.get('/', function(req, res) {
-  res.send('Hello, World!');
+  res.send('Git webhook successful!');
 });
 
-var server = app.listen(3000, function() {
+var server = app.listen(666, function() {
  
 console.log('Server listening');
  
